0ce462744637f900dedc3949f63d363132abfaa4
Robert Henderson <robert@nterpryz.com>
2018-04-27 02:09:51 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  address:string = '';
+  longitude:string = '';
+  latitude:string = '';
+  constructor(private httpClient:HttpClient){}
+  onKeyUp(event:any){
+    this.address = event.target.value
+  }
+  getCoordinates(){
+    this.httpClient.get('https://maps.googleapis.com/maps/api/geocode/json?address=${this.address}key=AIzaSyCtcfyY33kDeveLH1QyToCneZGabjFNhfw')
+    .subscribe(
+      (data:any []) => {
+        console.log(data)
+      }
+    )
+  }
 }
+
+//api key AIzaSyCtcfyY33kDeveLH1QyToCneZGabjFNhfw
